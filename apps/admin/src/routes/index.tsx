efefbfc145c5aa3d2cef810ea0f9b1dd866a21c0
EATSTEAK <me@eatsteak.dev>
2025-06-16 22:45:24 +0900

    lint: format

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -8,10 +8,12 @@ export const Route = createFileRoute("/")({
 function App() {
   return (
     <div className="text-center">
-      <header className={`
-        flex min-h-screen flex-col items-center justify-center bg-[#282c34]
-        text-[calc(10px+2vmin)] text-white
-      `}>
+      <header
+        className={`
+          flex min-h-screen flex-col items-center justify-center bg-[#282c34]
+          text-[calc(10px+2vmin)] text-white
+        `}
+      >
         <img
           src={logo}
           className={`
