cfb73d2aeedcde4c58ddb20588abbbf7a4fc8bfe
EATSTEAK <me@eatsteak.dev>
2025-06-17 00:31:35 +0900

    feat(ui): storybook setup

---
+++ packages/ui/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import viteReact from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "node:path";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [viteReact(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
