f7cb9f7f07e2be333cb0d7840164feaeb3a7dc75
EATSTEAK <me@eatsteak.dev>
2025-06-17 00:31:50 +0900

    feat(apps): tailwindcss for vite

--- apps/user/vite.config.ts
+++ apps/user/vite.config.ts
@@ -2,10 +2,15 @@ import { defineConfig } from "vitest/config";
 import viteReact from "@vitejs/plugin-react";
 import { resolve } from "node:path";
 import tanstackRouter from "@tanstack/router-plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [tanstackRouter({ autoCodeSplitting: true }), viteReact()],
+  plugins: [
+    tanstackRouter({ autoCodeSplitting: true }),
+    viteReact(),
+    tailwindcss(),
+  ],
   test: {
     globals: true,
     environment: "jsdom",
