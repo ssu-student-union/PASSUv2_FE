ce6ee518381fbb672d821a5d80adc7f91b65be11
qowldud <bjy6631@daum.net>
2025-06-19 00:23:32 +0900

    fix(storybook): support Node.js >22 by using createRequire for CJS compatibility

--- packages/ui/.storybook/main.ts
+++ packages/ui/.storybook/main.ts
@@ -1,14 +1,18 @@
 import type { StorybookConfig } from "@storybook/react-vite";
-
+import { createRequire } from "module";
 import { join, dirname } from "path";
 
 /**
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): any {
+
+const require = createRequire(import.meta.url);
+
+function getAbsolutePath(value: string): string {
   return dirname(require.resolve(join(value, "package.json")));
 }
+
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
