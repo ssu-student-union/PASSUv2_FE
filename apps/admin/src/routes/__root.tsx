a70bb0d6240865a4cf90628df6acccc112939524
qowldud <bjy6631@daum.net>
2025-06-28 19:48:37 +0900

    style: html, body, #app height 100% 설정

--- apps/admin/src/routes/__root.tsx
+++ apps/admin/src/routes/__root.tsx
@@ -15,7 +15,7 @@ function RootLayout() {
   );
   return (
     <>
-      <div className="flex">
+      <div className="flex h-full">
         {!isHideSidebar && (
           <div className={`max-w-104 min-w-80 basis-1/5`}>
             <SideBar />
