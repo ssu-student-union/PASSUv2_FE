3585a844a2d9e2453c8e08c6ea578090a859a5da
qowldud <bjy6631@daum.net>
2025-07-05 03:09:54 +0900

    feat: txt-* 유틸리티 tailwind-merge font-size 그룹 추가

--- packages/ui/src/utils.ts
+++ packages/ui/src/utils.ts
@@ -1,6 +1,30 @@
 import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { extendTailwindMerge } from "tailwind-merge";
+
+const customTwMerge = extendTailwindMerge({
+  extend: {
+    classGroups: {
+      "font-size": [
+        {
+          txt: [
+            "h1",
+            "h2",
+            "h3",
+            "h4",
+            "h5",
+            "h6",
+            "subtitle1",
+            "subtitle2",
+            "subtitle3",
+            "body1",
+            "body2",
+          ],
+        },
+      ],
+    },
+  },
+});
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return customTwMerge(clsx(inputs));
 }
