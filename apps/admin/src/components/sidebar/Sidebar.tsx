c4da3f613096a0b9ae9d35ff6cf6586f455e6c23
qowldud <bjy6631@daum.net>
2025-07-14 05:18:14 +0900

    refactor: Sidebar 컴포넌트 합성 방식으로 리팩토링

--- apps/admin/src/components/sidebar/Sidebar.tsx
+++ apps/admin/src/components/sidebar/Sidebar.tsx
@@ -1,26 +1,18 @@
-import type { SidebarButtonItem, SidebarListItem } from "@/types/sidebar";
 import { PassuLogo } from "@passu/ui/passu-logo";
-import SidebarNavItem from "./SidebarNavItem";
-import SidebarListSection from "./SidebarListSection";
+
+import { type ReactNode } from "react";
 
 interface SidebarProps {
-  buttons: SidebarButtonItem[];
-  list?: SidebarListItem[];
+  children: ReactNode;
 }
 
-export default function Sidebar({ buttons, list }: SidebarProps) {
+export default function Sidebar({ children }: SidebarProps) {
   return (
     <aside
       className={`flex max-w-104 min-w-85 basis-1/5 flex-col gap-12 px-10 pt-24`}
     >
       <PassuLogo className="w-full" />
-
-      <nav className="flex flex-col gap-3">
-        {buttons.map((item, idx) => (
-          <SidebarNavItem key={idx} item={item} />
-        ))}
-      </nav>
-      {list && <SidebarListSection list={list} />}
+      {children}
     </aside>
   );
 }
--- apps/admin/src/layouts/PageLayout.tsx
+++ apps/admin/src/layouts/PageLayout.tsx
@@ -1,4 +1,6 @@
 import Sidebar from "@/components/sidebar/Sidebar";
+import SidebarListSection from "@/components/sidebar/SidebarListSection";
+import SidebarNavItem from "@/components/sidebar/SidebarNavItem";
 import type { SidebarProps } from "@/types/sidebar";
 import type { ReactNode } from "react";
 
@@ -8,11 +10,19 @@ interface PageLayoutProps extends SidebarProps {
 
 export default function PageLayout({
   children,
-  ...sidebarProps
+  list,
+  buttons,
 }: PageLayoutProps) {
   return (
     <div className="flex h-full">
-      <Sidebar {...sidebarProps} />
+      <Sidebar>
+        <nav className="flex flex-col gap-3">
+          {buttons.map((item, idx) => (
+            <SidebarNavItem key={idx} item={item} />
+          ))}
+        </nav>
+        {list && <SidebarListSection list={list} />}
+      </Sidebar>
       <main className="flex-1">{children}</main>
     </div>
   );
