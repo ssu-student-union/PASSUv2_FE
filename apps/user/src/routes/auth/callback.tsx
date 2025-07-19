b8fb37e4cde097bafbd84a4faf4000568a6c0cf3
EATSTEAK <me@eatsteak.dev>
2025-07-20 04:59:40 +0900

    feat(user): view of `/auth/callback`

--- apps/user/src/routes/auth/callback.tsx
+++ apps/user/src/routes/auth/callback.tsx
@@ -2,8 +2,10 @@ import { createFileRoute, redirect } from "@tanstack/react-router";
 import { useSetAtom } from "jotai";
 import { accessTokenAtom } from "../../atoms/auth";
 import { useEffect } from "react";
+import { useMotionValue, animate } from "motion/react";
 import { z } from "zod";
 import { zodValidator } from "@tanstack/zod-adapter";
+import { Progress } from "@passu/ui/progress";
 
 const authCallbackSearchSchema = z.object({
   accessToken: z.string(),
@@ -24,10 +26,18 @@ export const Route = createFileRoute("/auth/callback")({
 function AuthCallback() {
   const { accessToken } = Route.useSearch();
   const setAccessToken = useSetAtom(accessTokenAtom);
+  const progressValue = useMotionValue(0);
+
+  useEffect(() => {
+    const controls = animate(progressValue, 100, { duration: 1 }); // Animate to 100% over 1 second
+    return () => controls.stop();
+  });
 
   useEffect(() => {
     if (accessToken) {
+      // TODO: Set the access token after verification with user-info API requests
       setAccessToken(accessToken);
+      // TODO: Add `to` query parameter to redirect URL
       // Redirect to home or a dashboard after setting the token
       // You might want to navigate to a specific page based on your app's logic
       window.location.href = "/";
@@ -35,8 +45,43 @@ function AuthCallback() {
   }, [accessToken, setAccessToken]);
 
   return (
-    <div>
-      <p>Processing authentication...</p>
+    <div
+      className={`
+        relative box-border flex size-full flex-col content-stretch items-center
+        justify-center gap-16 p-0
+      `}
+      data-name="View"
+      id="node-1267_1455"
+    >
+      <div
+        className={`
+          relative box-border flex w-60 shrink-0 flex-col content-stretch
+          items-center justify-center gap-16 px-0 pt-0 pb-[120px]
+        `}
+        data-name="Login"
+        id="node-1267_1456"
+      >
+        <div
+          className={`
+            relative w-full shrink-0 text-center text-[24px] leading-[0]
+            text-[rgba(0,0,0,0.8)] not-italic
+            font-['Pretendard:Regular',_sans-serif]
+          `}
+          id="node-1252_723"
+        >
+          <p className="block leading-[normal]">로그인 중</p>
+        </div>
+        <div
+          className={`
+            relative box-border flex w-full shrink-0 flex-col content-stretch
+            items-start justify-start gap-2 rounded bg-[#e2f2ff] p-0
+          `}
+          data-name="Load"
+          id="node-1252_724"
+        >
+          <Progress value={progressValue.get()} />
+        </div>
+      </div>
     </div>
   );
 }
