6c7a18ec708d7b543c4ab5555400f2a2234e2e7b
EATSTEAK <me@eatsteak.dev>
2025-07-20 07:46:05 +0900

    feat(user): attach query client to root

--- apps/user/src/routes/__root.tsx
+++ apps/user/src/routes/__root.tsx
@@ -1,10 +1,18 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+// QueryClient 인스턴스 생성
+const queryClient = new QueryClient();
 
 export const Route = createRootRoute({
   component: () => (
     <>
-      <Outlet />
+      <QueryClientProvider client={queryClient}>
+        <Outlet />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
       <TanStackRouterDevtools />
     </>
   ),
