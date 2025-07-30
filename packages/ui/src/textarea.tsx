e1d06a622f056446ab2311eba33a7c80b4bb9238
qowldud <bjy6631@daum.net>
2025-07-30 22:44:40 +0900

    fix: format 오류

--- packages/ui/src/textarea.tsx
+++ packages/ui/src/textarea.tsx
@@ -8,7 +8,7 @@ function Textarea({ className, ...props }: React.ComponentProps<"textarea">) {
       data-slot="textarea"
       className={cn(
         "border-input resize-none placeholder:text-muted-foreground aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive dark:bg-input/30 flex field-sizing-content min-h-16 w-full rounded-md border px-3 py-2 text-base shadow-xs transition-[color,box-shadow] outline-none focus:border-input disabled:cursor-not-allowed disabled:opacity-50 md:text-sm bg-gray-100",
-        className
+        className,
       )}
       {...props}
     />
