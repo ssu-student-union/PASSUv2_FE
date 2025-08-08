3e31b08da490a7e2f41754c783380de4491b3145
qowldud <bjy6631@daum.net>
2025-08-08 23:11:45 +0900

    fix: pnpm format

--- packages/ui/src/input.tsx
+++ packages/ui/src/input.tsx
@@ -11,7 +11,7 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "file:text-foreground placeholder:text-gray-400 selection:bg-primary selection:text-primary-foreground flex h-12 w-full min-w-0 rounded-t-lg border-0 border-b border-gray-400 bg-gray-100 px-4 py-3 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50",
         "focus-visible:bg-white focus-visible:border-primary focus-visible:ring-0",
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-        className
+        className,
       )}
       {...props}
     />
