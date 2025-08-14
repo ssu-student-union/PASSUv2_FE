320efd490e157d92cd6ffeb22360206d56431717
qowldud <bjy6631@daum.net>
2025-08-14 16:19:51 +0900

    style: 상품수령명단 테이블 border 값 수정

--- apps/admin/src/components/result/PrintableList.tsx
+++ apps/admin/src/components/result/PrintableList.tsx
@@ -31,26 +31,26 @@ export const PrintableList = ({ ref }: PrintableListProps) => {
         `}
       >
         <thead>
-          <tr>
-            <th className="border-2 border-black p-2">순번</th>
-            <th className="border-2 border-black p-2">이름</th>
-            <th className="border-2 border-black p-2">학과</th>
-            <th className="border-2 border-black p-2">학번</th>
-            <th className="border-2 border-black p-2">종류</th>
-            <th className="border-2 border-black p-2">수량</th>
-            <th className="border-2 border-black p-2">서명 또는 날인</th>
+          <tr className="border-t-3 border-black">
+            <th className="border-3 border-black p-2">순번</th>
+            <th className="border-3 border-black p-2">이름</th>
+            <th className="border-3 border-black p-2">학과</th>
+            <th className="border-3 border-black p-2">학번</th>
+            <th className="border-3 border-black p-2">종류</th>
+            <th className="border-3 border-black p-2">수량</th>
+            <th className="border-3 border-black p-2">서명 또는 날인</th>
           </tr>
         </thead>
         <tbody>
           {data.map((row) => (
             <tr key={row.id}>
-              <td className="h-10 border-2 border-black p-2">{row.id}</td>
-              <td className="border-2 border-black p-2"></td>
-              <td className="border-2 border-black p-2"></td>
-              <td className="border-2 border-black p-2"></td>
-              <td className="border-2 border-black p-2"></td>
-              <td className="border-2 border-black p-2"></td>
-              <td className="border-2 border-black p-2"></td>
+              <td className="h-10 border-3 border-black p-2">{row.id}</td>
+              <td className="border-3 border-black p-2"></td>
+              <td className="border-3 border-black p-2"></td>
+              <td className="border-3 border-black p-2"></td>
+              <td className="border-3 border-black p-2"></td>
+              <td className="border-3 border-black p-2"></td>
+              <td className="border-3 border-black p-2"></td>
             </tr>
           ))}
         </tbody>
