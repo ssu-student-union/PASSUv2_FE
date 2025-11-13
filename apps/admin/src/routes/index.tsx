470fe9bb10a26536fd7ed04d9cb7905275fc75e0
qowldud <bjy6631@daum.net>
2025-11-13 13:22:28 +0900

    chore: 사용자 이름 api 주석 제거

--- apps/admin/src/routes/index.tsx
+++ apps/admin/src/routes/index.tsx
@@ -21,8 +21,8 @@ export const Route = createFileRoute("/")({
 });
 
 function App() {
-  // const { data } = useUserInfo();
-  // const userName = data?.data?.name ?? data?.data?.major;
+  const { data } = useUserInfo();
+  const userName = data?.data?.name ?? data?.data?.major;
 
   const handleLogout = () => {
     localStorage.removeItem("accessToken");
@@ -83,7 +83,7 @@ function App() {
                 lg:text-4xl
               `}
             >
-              {/* {userName} */}
+              {userName}
             </span>
             <span
               className={`
