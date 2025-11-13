8433da8c56660cc5e7e4f954af958d8ed498bf54
qowldud <bjy6631@daum.net>
2025-11-13 13:08:36 +0900

    style: admin 전체 스크롤바 숨김 처리 추가

--- apps/admin/src/components/home/EventAccordion.tsx
+++ apps/admin/src/components/home/EventAccordion.tsx
@@ -30,6 +30,7 @@ export const EventAccordion = ({ type }: EventAccordionProps) => {
       : type === EventStatus.PAUSE
         ? "진행중인 행사"
         : "완료된 행사";
+
   const textColor: string =
     type === EventStatus.AFTER ? "text-gray-600" : "text-gray-900";
 
