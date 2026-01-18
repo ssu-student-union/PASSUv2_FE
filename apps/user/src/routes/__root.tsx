3e380d64fce6c9afa533ba92339c243596fa390a
Hyomin Koo <me@eatsteak.dev>
2026-01-18 16:13:23 +0900

    feat(user): replace error boundary with message display

--- apps/user/src/routes/__root.tsx
+++ apps/user/src/routes/__root.tsx
@@ -1,8 +1,13 @@
 import { Outlet, createRootRoute } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ErrorBoundary } from "../components/ErrorBoundary";
+import {
+  QueryClient,
+  QueryClientProvider,
+  QueryErrorResetBoundary,
+} from "@tanstack/react-query";
+import { ErrorBoundary } from "@suspensive/react";
+import { ErrorFallback } from "../components/ErrorFallback";
 
 // QueryClient 인스턴스 생성
 const queryClient = new QueryClient();
@@ -11,9 +16,16 @@ export const Route = createRootRoute({
   component: () => (
     <>
       <QueryClientProvider client={queryClient}>
-        <ErrorBoundary>
-          <Outlet />
-        </ErrorBoundary>
+        <QueryErrorResetBoundary>
+          {({ reset }) => (
+            <ErrorBoundary
+              onReset={reset}
+              fallback={(props) => <ErrorFallback {...props} />}
+            >
+              <Outlet />
+            </ErrorBoundary>
+          )}
+        </QueryErrorResetBoundary>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
       <TanStackRouterDevtools />
